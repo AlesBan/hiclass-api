48f8a1b5c06a9785cd0c3893ddd0600a32888b35
User <alesban36@gmail.com>
2024-04-02 23:12:01 +0300

    update context

--- HiСlass.Backend/HiClass.Infrastructure/obj/Debug/net6.0/HiClass.Infrastructure.AssemblyInfo.cs
+++ HiСlass.Backend/HiClass.Infrastructure/obj/Debug/net6.0/HiClass.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiClass.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0704fb853cc23a648995a27d2264b4a1a878ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e3782bcde13d7a950113a5606a04f7a68f0366")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiClass.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiClass.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
